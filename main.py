3c1864d2363e30a0f8413933146e3f459d763fd5
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-06-20 03:16:48 +0200

    moved clustering out of view code

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ class AppState(HasTraits):
     video_path = Unicode(allow_none=True)
     reference_frame = Instance(np.ndarray, allow_none=True)
     crop = Instance(Crop, allow_none=True)
+    selected_frames = Instance(np.ndarray, allow_none=True)
 
     @observe('video_path')
     def load_video(self, changed):
@@ -56,11 +57,25 @@ class AppState(HasTraits):
         shape = self.reference_frame.shape
         self.crop = Crop(x0=0, x1=shape[1], x1_max=shape[1], y0=0, y1=shape[0], y1_max=shape[0])
 
+    def extract_frames_via_kmeans(self, every_n: int = 50, n_clusters: int = 15):
+        # Read the Starting Frames to Analyze From the Video, cropping the frames to match the reference
+        video = VideoReader(filename=model.video_path)
+        frames = np.array(list(video.read_frames(step=every_n)))
+
+        # Crop Frames to Match Reference Frame
+        crop = model.crop
+        frames = frames[:, crop.y0:crop.y1, crop.x0:crop.x1]
+
+        # Extract only a Selection of Frames after clustering them using KMeans
+        selected_frame_indices = select_subset_frames_kmeans(frames=frames[:, :, :, 0], n_clusters=n_clusters)
+        selected_frames = frames[selected_frame_indices]
+        self.selected_frames = selected_frames
+
 
 model = AppState()
 
 
-### Load Reference Frame
+### Update Model from Widget
 @magicgui(
     video_path={"label": "Pick a Video File:"},
     x0={'label': 'x0', 'widget_type': 'Slider'},
@@ -74,6 +89,20 @@ def reference_frame_extraction_widget(video_path: Path = None, x0=0, x1=1, y0=0,
     model.crop = Crop(x0=x0, x1=x1, y0=y0, y1=y1)
 
 
+@magicgui(
+    every_n={"label": "Read Every N Frames"},
+    n_clusters={"label": "N Clusters"},
+    call_button="Extract Frames", 
+)
+def multiframe_extraction_widget(
+    every_n: int = 200,
+    n_clusters: int = 10,
+    # progress=5,
+):
+    model.extract_frames_via_kmeans(every_n=every_n, n_clusters=n_clusters)
+            
+
+    
 # Update View from Model
 @model.observe
 def view_reference_frame(changed):
@@ -90,44 +119,17 @@ def view_reference_frame(changed):
         reference_frame_extraction_widget.y0.value = crop.y0
         reference_frame_extraction_widget.y1.value = crop.y1
     elif changed['name'] == 'crop' and 'Reference Frame' in viewer.layers:
-        layer = viewer.layers['Reference Frame']
         crop = model.crop
         reference_frame_extraction_widget.x0.value = crop.x0
         reference_frame_extraction_widget.x1.value = crop.x1
         reference_frame_extraction_widget.y0.value = crop.y0
         reference_frame_extraction_widget.y1.value = crop.y1
+        layer = viewer.layers['Reference Frame']
         layer.data = model.reference_frame[crop.y0:crop.y1, crop.x0:crop.x1]
+    elif changed['name'] == 'selected_frames':
+        viewer.add_image(model.selected_frames)
 
 
-@magicgui(
-    video_path={"label": "Pick a Video File:"},
-    every_n={"label": "Read Every N Frames"},
-    n_clusters={"label": "N Clusters"},
-    call_button="Extract Frames", 
-)
-def multiframe_extraction_widget(
-    video_path: Path = Path(r"C:\Users\nickdg\Projects\WaspTracker\data\raw\jwasp0.avi"),
-    every_n: int = 200,
-    n_clusters: int = 10,
-    # progress=5,
-):
-
-    # Read the Starting Frames to Analyze From the Video, cropping the frames to match the reference
-    video = VideoReader(filename=video_path)
-    frames = np.array(list(video.read_frames(step=every_n)))
-
-    # Crop Frames to Match Reference Frame
-    x0, x1, y0, y1 = model.x0, model.x1, model.y0, model.y1
-    frames = frames[:, y0:y1, x0:x1]
-
-    # Extract only a Selection of Frames after clustering them using KMeans
-    selected_frame_indices = select_subset_frames_kmeans(frames=frames, n_clusters=n_clusters)
-    selected_frames = frames[selected_frame_indices]
-
-    # Finish: Send Data to Napari Viewer
-    napari.current_viewer().add_image(selected_frames)
-
-    
 
 
 if __name__ == '__main__':
