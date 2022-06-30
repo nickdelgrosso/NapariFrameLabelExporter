12da525b35baed2d5d41c1e1850d5327ca06f66f
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-06-30 21:26:26 +0200

    models subpackage

---
+++ gui/models/__init__.py
@@ -0,0 +1,2 @@
+from .app import AppState
+from .crop import CropState
--- gui/models/app.py
+++ gui/models/app.py
@@ -8,43 +8,9 @@ from workflows import ExtractFramesResult, Progress, extract_frames, Crop
 from readers import VideoReader
 
 from .utils import PrintableTraits
+from .crop import CropState
 
 # Model
-class CropState(HasTraits, PrintableTraits):
-    x0 = Int(default_value=0)
-    x1 = Int(default_value=50)
-    x_max = Int(default_value=100)
-    y0 = Int(default_value=0)
-    y1 = Int(default_value=60)
-    y_max = Int(default_value=80)
-
-    @validate('x0')
-    def _check_x0(self, proposal):
-        x0 = proposal['value']
-        if x0 >= self.x1:
-            return self.x0
-        return x0
-
-    @validate('x1')
-    def _check_x1(self, proposal):
-        x1 = proposal['value']
-        if self.x0 >= x1:
-            return self.x1
-        return x1
-
-    @validate('y0')
-    def _check_y0(self, proposal):
-        y0 = proposal['value']
-        if y0 >= self.y1:
-            return self.y0
-        return y0
-
-    @validate('y1')
-    def _check_y1(self, proposal):
-        y1 = proposal['value']
-        if self.y0 >= y1:
-            return self.y1
-        return y1
         
 
 class AppState(HasTraits, PrintableTraits):
